dbbfc7dccc75e30bca465a5b00dfcd8efd9048c6
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-10-20 19:39:37 -0500

    the modxule for the social media dashboard

--- ng-client/src/app/modules/social-media-dashboard/social-media-dashboard.module.ts
+++ ng-client/src/app/modules/social-media-dashboard/social-media-dashboard.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TwitterFeedComponent } from './twitter-feed/twitter-feed.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TwitterFeedComponent
+  ],
   imports: [
     CommonModule
   ]
